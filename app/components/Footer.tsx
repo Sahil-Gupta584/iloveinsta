2c482818e6b675bd2ebf980f7ed0fc9b30199416
Sahil Gupta <guptas3067@gmail.com>
2024-12-20 14:44:18 +0530

    fix deploy issue

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="mt-12 text-center text-sm text-gray-500">
       <p>Note: This tool is for educational purposes only.</p>
-      <p>Please respect Instagram's terms of service and content creators' rights.</p>
+      <p>Please respect Instagram&apos;s terms of service and content creators&apos; rights.</p>
     </footer>
   );
 }
